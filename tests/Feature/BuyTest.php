27090b52e0d507324528e03956e26498f952d95e
Jairo Pimentel <jairogpa8@gmail.com>
2021-05-19 02:35:06 -0400

    Finish Test

--- tests/Feature/BuyTest.php
+++ tests/Feature/BuyTest.php
@@ -5,7 +5,9 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Cache;
 use App\Product;
+use App\Order;
 use Tests\TestCase;
 
 class BuyTest extends TestCase
@@ -22,16 +24,16 @@ class BuyTest extends TestCase
         $response->assertStatus(200);
     }
     
-    public function testBuyGood()
+    public function testSelectProductGood()
     {
         $product = Product::orderBy('name')->first();
         
         $response = $this->get('/buy/'.$product->id);
         
-        $response->assertStatus(200);
+        $response->assertStatus(200)->assertSee('Producto agregado al carro');
     }
     
-    public function testBuyNotFound()
+    public function testSelectProductNotFound()
     {        
         $response = $this->get('/buy/100');
         
@@ -42,7 +44,7 @@ class BuyTest extends TestCase
     {
         $response = $this->get('/order/create');
         
-        $response->assertStatus(200);
+        $response->assertStatus(200)->assertSee('Información del comprador');
     }
     
     
@@ -69,10 +71,51 @@ class BuyTest extends TestCase
             '_token' => csrf_token(),
             'customer_name' => Str::random(10),
             'customer_email' => Str::random(10),
-            'customer_mobile' => random_int(300000000, 9999999999),
+            'customer_mobile' => random_int(30000, 99999),
+            'amount' => $product->price
+        ]);        
+        
+        $response->assertStatus(422);
+    }
+    
+    public function testOrderBuy()
+    {
+        $product = Product::orderBy('name')->first();        
+        Cache::put('products', array($product->id)); 
+        
+        $response = $this->from('/order/resume')->post('/order/pay', [
+            '_token' => csrf_token(),
+            'customer_name' => Str::random(10),
+            'customer_email' => Str::random(10).'@gmail.com',
+            'customer_mobile' => random_int(3000000000, 9999999999),
             'amount' => $product->price
         ]);
         
-        $response->assertStatus(422);
+        $order = Order::orderBy('id', 'desc')->first();
+        $url = $order->placetopay_url;
+        
+        $response->assertRedirect($url);
+    }
+    
+    public function testResponse()
+    {
+        $order = Order::orderBy('id', 'desc')->first();
+        $response = $this->json('GET', '/order/response?reference='.$order->id);
+        
+        $response->assertStatus(200);
+    }
+    
+    public function testShowOrder()
+    {
+        $order = Order::orderBy('id', 'desc')->first();
+        $response = $this->json('GET', '/order/'.$order->id);
+        
+        $response->assertStatus(200);
+    }
+    
+    public function testListOrder(){
+        $response = $this->json('GET', '/order/list');
+        
+        $response->assertStatus(200);
     }
 }
